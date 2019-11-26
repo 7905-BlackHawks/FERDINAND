f3264109b68ef27d4ea07526f771404b79af6f02
7905-BlackHawks <isaiahbailey02@gmail.com>
2019-11-26 16:59:50 -0700

    update

--- src/main/java/frc/robot/commands/turn180auto.java
+++ src/main/java/frc/robot/commands/turn180auto.java
@@ -20,27 +20,42 @@ public class turn180auto extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    double time = Timer.getFPGATimestamp();
+    // double time = Timer.getFPGATimestamp();
 
-    if (time < 2) {
-      Robot.DriveTrain.left1.set(0.3);
-      Robot.DriveTrain.left2.set(0.3);
-      Robot.DriveTrain.right1.set(-0.3);
-      Robot.DriveTrain.right2.set(-0.3);
-    } else {
-      Robot.DriveTrain.left1.set(0);
-      Robot.DriveTrain.left2.set(0);
-      Robot.DriveTrain.right1.set(0);
-      Robot.DriveTrain.right2.set(0);
+    // if (time < 2) {
+    // Robot.DriveTrain.left1.set(0.3);
+    // Robot.DriveTrain.left2.set(0.3);
+    // Robot.DriveTrain.right1.set(-0.3);
+    // Robot.DriveTrain.right2.set(-0.3);
+    // } else {
+    // Robot.DriveTrain.left1.set(0);
+    // Robot.DriveTrain.left2.set(0);
+    // Robot.DriveTrain.right1.set(0);
+    // Robot.DriveTrain.right2.set(0);
 
-
-
+    // }
+    Robot.DriveTrain.left1.set(0.3);
+    Robot.DriveTrain.left2.set(0.3);
+    Robot.DriveTrain.right1.set(-0.3);
+    Robot.DriveTrain.right2.set(-0.3);
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      
+      e.printStackTrace();
     }
+    Robot.DriveTrain.left1.set(0);
+    Robot.DriveTrain.left2.set(0);
+    Robot.DriveTrain.right1.set(0);
+    Robot.DriveTrain.right2.set(0);
+
+
 
 
   }
@@ -54,7 +69,7 @@ public class turn180auto extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    
+
   }
 
   // Called when another command which requires one or more of the same
